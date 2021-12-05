a3cebd8634f502a67f2bbdc620ea5acc31466545
Admir Alicic <admir.alicic@gmail.com>
2021-12-05 17:42:04 +0100

    Setup MikroORM

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import microConfig from "./mikro-orm.config";
+// import { Post } from "./entities/Post";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+  // const post = orm.em.create(Post, { title: "my third post" });
+  // await orm.em.persistAndFlush(post);
+};
+
+main();
+
 console.log("hello world!!!");

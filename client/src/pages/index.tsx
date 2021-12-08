288b50ac79775e28bae7c4e1833e11ee35f5dbc0
Admir Alicic <admir.alicic@gmail.com>
2021-12-08 21:14:54 +0100

    Add navbar

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,3 +1,9 @@
-const Index = () => <div>Hello !!!!!!!</div>;
+import { NavBar } from "../components/NavBar";
+const Index = () => (
+  <>
+    <NavBar />
+    <div>Hello !!!!!!!</div>
+  </>
+);
 
 export default Index;
